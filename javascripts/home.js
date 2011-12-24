ebfc2ad9575b77b939f4ed42b77f921a53d3acc9
tiantian <maplebeats@gmail.com>
2011-12-24 23:06:58 +0800

    rm the old js,and add the new js

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,70 +1,137 @@
-document.observe("dom:loaded", function() {
-  var search_field = $('search');
-  var search_label = $$('#search_area label').first();
-  var clear_search = $$('#search_area img').first();
-  var tweets = $('tweets');
-  var body = $$('body').first();
-  var json_search = new JSONSearch({
-    fields: {
-      title: 'infix',
-      summary: 'word_prefix',
-      content: 'word_prefix'
-    },
-    ranks: {
-      title: 3,
-      summary: 2,
-      content: 1
-    }
-  });
-  
-  var search = function(e) {
-    if (e && e.keyCode == 27) {
-      clear();
-    }
-    var query = search_field.value;
-    var results;
-    if (query.blank()) {
-      results = posts;
-    } else {
-      results = json_search.getResults(query, posts);
-    }
-
-    if (!query.blank()) {
-      body.addClassName('searching');
-      search_label.hide();
-    } else {
-      body.removeClassName('searching');
-    }
-    $('result_count').update('(' + results.length + ')');
-    
-    $('search_results').update(results.collect(function(post) {
-      return('<li><a href="' + post.url + '">' + post.title + '</a><p>' + post.summary + '</p></li>');
-    }).join(''));
-  };
-  
-  if (search_field.value.blank()) {
-    search_label.show();
-  }
-
-  search_field.observe('keyup', search);
-  
-  search_field.observe('focus', function(e) {
-    search_label.hide();
-  });
-
-  search_field.observe('blur', function(e) {
-    if (search_field.value.blank()) {
-      search_label.show();
-    }
-  });
-
-  var clear = function() {
-    search_field.value = '';
-    search_label.show();
-    search();
-    search_field.blur();
-  }
-  clear_search.observe('click', clear);
-  search();
-});
-$('search_area').show();
+function $(d){ 
+return document.getElementById(d); 
+} 
+// set or get the current display style of the div 
+function dsp(d,v){ 
+if(v==undefined){ 
+return d.style.display; 
+}else{ 
+d.style.display=v; 
+} 
+} 
+// set or get the height of a div. 
+function sh(d,v){ 
+// if you are getting the height then display must be block to return the absolute height 
+if(v==undefined){ 
+if(dsp(d)!='none'&& dsp(d)!=''){ 
+return d.offsetHeight; 
+} 
+viz = d.style.visibility; 
+d.style.visibility = 'hidden'; 
+o = dsp(d); 
+dsp(d,'block'); 
+r = parseInt(d.offsetHeight); 
+dsp(d,o); 
+d.style.visibility = viz; 
+return r; 
+}else{ 
+d.style.height=v; 
+} 
+} 
+/* 
+* Variable 'S' defines the speed of the accordian 
+* Variable 'T' defines the refresh rate of the accordian 
+*/ 
+s=7; 
+t=10; 
+//Collapse Timer is triggered as a setInterval to reduce the height of the div exponentially. 
+function ct(d){ 
+d = $(d); 
+if(sh(d)>0){ 
+v = Math.round(sh(d)/d.s); 
+v = (v<1) ? 1 :v ; 
+v = (sh(d)-v); 
+sh(d,v+'px'); 
+d.style.opacity = (v/d.maxh); 
+d.style.filter= 'alpha(opacity='+(v*100/d.maxh)+');'; 
+}else{ 
+sh(d,0); 
+dsp(d,'none'); 
+clearInterval(d.t); 
+} 
+} 
+//Expand Timer is triggered as a setInterval to increase the height of the div exponentially. 
+function et(d){ 
+d = $(d); 
+if(sh(d)<d.maxh){ 
+v = Math.round((d.maxh-sh(d))/d.s); 
+v = (v<1) ? 1 :v ; 
+v = (sh(d)+v); 
+sh(d,v+'px'); 
+d.style.opacity = (v/d.maxh); 
+d.style.filter= 'alpha(opacity='+(v*100/d.maxh)+');'; 
+}else{ 
+sh(d,d.maxh); 
+clearInterval(d.t); 
+} 
+} 
+// Collapse Initializer 
+function cl(d){ 
+if(dsp(d)=='block'){ 
+clearInterval(d.t); 
+d.t=setInterval('ct("'+d.id+'")',t); 
+} 
+} 
+//Expand Initializer 
+function ex(d){ 
+if(dsp(d)=='none'){ 
+dsp(d,'block'); 
+d.style.height='0px'; 
+clearInterval(d.t); 
+d.t=setInterval('et("'+d.id+'")',t); 
+} 
+} 
+// Removes Classname from the given div. 
+function cc(n,v){ 
+s=n.className.split(/\s+/); 
+for(p=0;p<s.length;p++){ 
+if(s[p]==v+n.tc){ 
+s.splice(p,1); 
+n.className=s.join(' '); 
+break; 
+} 
+} 
+} 
+//Accordian Initializer 
+function Accordian(d,s,tc){ 
+// get all the elements that have id as content 
+l=$(d).getElementsByTagName('div'); 
+c=[]; 
+for(i=0;i<l.length;i++){ 
+h=l[i].id; 
+if(h.substr(h.indexOf('-')+1,h.length)=='content'){c.push(h);} 
+} 
+sel=null; 
+//then search through headers 
+for(i=0;i<l.length;i++){ 
+h=l[i].id; 
+if(h.substr(h.indexOf('-')+1,h.length)=='header'){ 
+d=$(h.substr(0,h.indexOf('-'))+'-content'); 
+d.style.display='none'; 
+d.style.overflow='hidden'; 
+d.maxh =sh(d); 
+d.s=(s==undefined)? 7 : s; 
+h=$(h); 
+h.tc=tc; 
+h.c=c; 
+// set the onclick function for each header. 
+h.onclick = function(){ 
+for(i=0;i<this.c.length;i++){ 
+cn=this.c[i]; 
+n=cn.substr(0,cn.indexOf('-')); 
+if((n+'-header')==this.id){ 
+ex($(n+'-content')); 
+n=$(n+'-header'); 
+cc(n,'__'); 
+n.className=n.className+' '+n.tc; 
+}else{ 
+cl($(n+'-content')); 
+cc($(n+'-header'),''); 
+} 
+} 
+} 
+if(h.className.match(/selected+/)!=undefined){ sel=h;} 
+} 
+} 
+if(sel!=undefined){sel.onClick();} 
+} 
