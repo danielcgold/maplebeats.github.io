1890cfb8531611d173527c95cb426373b75a9b02
Tian Tian <maplebeats@gmail.com>
2012-02-16 19:18:20 +0800

    add a html5 player

---
+++ javascripts/musiclist.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	var myCirclePlayer = new CirclePlayer("#jquery_jplayer_1",
+	{
+		m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+		oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+	}, {
+		cssSelectorAncestor: "#cp_container_1",
+		swfPath: "js",
+		wmode: "window"
+	});
+});
