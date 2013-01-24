af7a5cbb90c509cfbdb2fa925e4bb486ae66ae61
枫叶饭团 <maplebeats@gmail.com>
2013-01-24 22:06:00 +0800

    rewrite index.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-window.setTimeout(function(){
+setTimeout(function(){
     window.location = '/blog';
 },
-2000)
+5000);
+
+var time = document.getElementById('time');
+var m = 4;
+setInterval(function(){
+    time.innerHTML = (m--);
+},1000);
